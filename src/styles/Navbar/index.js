30b25a9d28404612b759faac0ad7eeaf76ce9f45
Kajal Raj <raj11kajal@gmail.com>
2025-01-07 10:38:24 +0530

    update logo into text name

--- src/styles/Navbar/index.js
+++ src/styles/Navbar/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Logo = styled.div`
     font-family: cursive;
     font-weight: bold;
-    font-size: 32px;
+    font-size: 25px;
     opacity: 0.9;
     display: flex;
     flex-direction: column;
